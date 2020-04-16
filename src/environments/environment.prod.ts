662f6106c32372f3eeb72085797388757645a1b1
marioarb97 <marioarb.97@gmail.com>
2020-04-15 22:05:53 -0500

    menor: version 1 para firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   authorization: 'Basic ' + btoa('AsisgeApp:clave123'),
-  api_endpoint: 'http://localhost:8080/api/v1', // TODO: cambiar por enlace a heroku o aws
+  api_endpoint: 'https://test-bd-elenchos.herokuapp.com/api/v1', // TODO: cambiar por enlace a heroku o aws
   token_path: 'oauth/token',
   grant_type: 'password',
 
